daaa9ff0b570ab9f7e3b04f04e617e0f8531426e
Kunal SAgar <kunalsagar1999@gmail.com>
2020-09-01 19:39:29 +0530

    init

---
+++ Source/wheelSpin/wheelSpinGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "wheelSpinGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WHEELSPIN_API AwheelSpinGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
